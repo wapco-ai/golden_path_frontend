ba0a0b60d94c83a39e4bb1db99d29e120dc97bac
wapco <wappo.dev@gmail.com>
2025-11-18 21:22:26 +0330

    Fix invalid paint property for fill layer

--- src/config/vectorTiles.js
+++ src/config/vectorTiles.js
@@ -90,8 +90,7 @@ export const haramVectorTileConfig = [
     paint: {
       'fill-color': 'rgba(0, 0, 0, 0)',
       'fill-outline-color': '#000000',
-      'fill-opacity': 1,
-      'line-width': 2
+      'fill-opacity': 1
     }
   },
   {
