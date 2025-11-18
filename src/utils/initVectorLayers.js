2fa6e67b32e21dbe64f287bc3a60a35a9676d7d6
gisonline <oskooei@gmail.com>
2025-11-18 16:19:18 +0330

    mbjj

--- src/utils/initVectorLayers.js
+++ src/utils/initVectorLayers.js
@@ -76,7 +76,7 @@ const ensureSourcesAndLayers = (map) => {
       if (meta.isDynamic) {
         const source = map.getSource(sourceId);
         if (source && typeof source.setTiles === 'function') {
-          source.setTiles([meta.tileUrlResolver({ floor: currentFloor })]);
+          source.setTiles([meta.tileUrlResolver({p_floor: currentFloor })]);
         }
       }
       return;
@@ -84,7 +84,7 @@ const ensureSourcesAndLayers = (map) => {
 
     map.addSource(sourceId, {
       type: 'vector',
-      tiles: [meta.tileUrlResolver({ floor: currentFloor })],
+      tiles: [meta.tileUrlResolver({ p_floor: currentFloor })],
       minzoom: meta.minzoom,
       maxzoom: meta.maxzoom
     });
