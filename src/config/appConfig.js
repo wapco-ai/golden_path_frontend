c4f2cfeb65a93005470b9f5d21a38b51f303d118
gisonline <oskooei@gmail.com>
2025-12-04 10:02:02 +0330

    Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -7,8 +7,7 @@ const inferHostedBaseUrl = () => {
 
   // When running the PWA from a production host we want to hit the same origin.
   if (hostname && hostname !== 'localhost' && hostname !== '127.0.0.1') {
-    return 'http://185.238.46.59:8085';
-    // return origin;
+    return origin;
   }
 
   return null;
@@ -16,21 +15,19 @@ const inferHostedBaseUrl = () => {
 
 const inferHostedTileBaseUrl = () => {
   if (typeof window === 'undefined' || !window?.location?.origin) {
-    return 'http://185.238.46.59:8085/tiles';
-    //return 'http://localhost:8080/tiles';
+    return 'http://localhost:8080/tiles';
   }
 
   const { origin, hostname } = window.location;
 
   if (hostname && hostname !== 'localhost' && hostname !== '127.0.0.1') {
-    // return `${origin.replace(/\/$/, '')}/tiles`;
-    return 'http://185.238.46.59:8085/tiles';
+    return `${origin.replace(/\/$/, '')}/tiles`;
   }
-  return 'http://185.238.46.59:8085/tiles';
-  //return 'http://localhost:8080/tiles';
+
+  return 'http://localhost:8080/tiles';
 };
 
-const defaultApiBaseUrl = inferHostedBaseUrl() || 'http://185.238.46.59:8085';
+const defaultApiBaseUrl = inferHostedBaseUrl() || 'http://localhost:8080';
 const defaultTileBaseUrl = inferHostedTileBaseUrl();
 const defaultRoutingRouteUrl = `${defaultApiBaseUrl}/api/v1/routing/route`;
 const defaultLanguagesUrl = `${defaultApiBaseUrl}/api/v1/languages`;
