c7d928d553152ae42f0226352eeb35ec24a83be0
wapco <wappo.dev@gmail.com>
2025-12-05 20:14:12 +0330

    Make events modal fullscreen with navigation

--- src/pages/MapBegin.jsx
+++ src/pages/MapBegin.jsx
@@ -1080,6 +1080,24 @@ const MapBeginPage = () => {
                           <span className="modal-meta-divider">•</span>
                           <span>{event.time}</span>
                         </div>
+                        <div className="events-modal-actions">
+                          <button
+                            className="place-action-btn events-modal-nav-btn"
+                            onClick={() => navigate('/fs')}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="16"
+                              height="16"
+                              viewBox="0 0 24 24"
+                              fill="currentColor"
+                            >
+                              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                              <path d="M11.092 2.581a1 1 0 0 1 1.754 -.116l.062 .116l8.005 17.365c.198 .566 .05 1.196 -.378 1.615a1.53 1.53 0 0 1 -1.459 .393l-7.077 -2.398l-6.899 2.338a1.535 1.535 0 0 1 -1.52 -.231l-.112 -.1c-.398 -.386 -.556 -.954 -.393 -1.556l.047 -.15l7.97 -17.276z" />
+                            </svg>
+                            {intl.formatMessage({ id: 'navigate' })}
+                          </button>
+                        </div>
                       </div>
                     </div>
                   ))}
