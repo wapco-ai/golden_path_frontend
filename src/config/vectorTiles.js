975eefc8ea3d51f51f7f2aef3c684affec0cfb13
gisonline <oskooei@gmail.com>
2025-12-08 16:05:20 +0330

    Update vectorTiles.js

--- src/config/vectorTiles.js
+++ src/config/vectorTiles.js
@@ -18,9 +18,9 @@ const MESH_TRIANGLES_SOURCE_LAYER = 'public.vw_mesh_triangles';
 const MESH_TRIANGLES_TILE_BASE = `${TILE_BASE_URL}/${MESH_TRIANGLES_SOURCE_LAYER}/{z}/{x}/{y}.pbf`;
 const ROUTING_EDGES_STATIC_SOURCE_LAYER = 'public.routing_edges_static';
 const ROUTING_EDGES_STATIC_BASE = `${TILE_BASE_URL}/${ROUTING_EDGES_STATIC_SOURCE_LAYER}/{z}/{x}/{y}.pbf`;
-const DOORS_ACCESS_POINT_SOURCE_LAYER = 'public.door_access_points';
+const DOORS_ACCESS_POINT_SOURCE_LAYER = 'public.fn_door_access_points_mvt';
 // The MVT layer name returned by the function omits the schema prefix
-export const DOORS_ACCESS_POINT_LAYER_NAME = 'fn_door_access_points_mvt';
+export const DOORS_ACCESS_POINT_LAYER_NAME = 'door_access_points';
 const DOORS_ACCESS_POINT_BASE = `${TILE_BASE_URL}/${DOORS_ACCESS_POINT_SOURCE_LAYER}/{z}/{x}/{y}.pbf`;
 
 const buildDoorAccessPointsTileUrlFactory = () => buildFloorOnlyTileUrlFactory(DOORS_ACCESS_POINT_BASE);
