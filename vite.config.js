1fd4965d443b93ef2c13eebc2e118b481c165a00
gisonline <oskooei@gmail.com>
2025-12-21 00:23:43 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   // base: process.env.BASE_URL || '/',
-  base: isProd ? '/golden_path_backend/' : '/',
+  base: isProd ? '/' : '/',
   // Add this for PWA  
   publicDir: 'public',
   resolve: {
@@ -33,8 +33,8 @@ export default defineConfig({
         theme_color: '#ffffff',  
         background_color: '#f0f2f5',  
         display: 'standalone',  
-        scope: '/golden_path_backend/',  
-        start_url: '/golden_path_backend/',  
+        scope: '/',  
+        start_url: '/',  
         icons: [  
           {  
             src: '/icons/pwa-192x192.png',  
