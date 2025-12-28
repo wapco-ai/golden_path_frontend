d3289ebd5b91516ee6a9b811cc72543a2631cf2c
Wapco-tech <wappo.dev@gmail.com>
2025-12-28 12:23:31 +0330

    شیش
    ی

--- src/AdminPanel/Admins.jsx
+++ src/AdminPanel/Admins.jsx
@@ -167,7 +167,7 @@ const Admins = () => {
     setSelectedAdmin(admin);
     setIsDeleteModalOpen(true);
   };
-  
+
 
   // Handle role selection toggle
   const handleRoleToggle = (role) => {
@@ -341,7 +341,13 @@ const Admins = () => {
                   <tr key={admin.id}>
                     <td>
                       <div className="user-profile-cell">
-                        <div className="profile-image-small"></div>
+                        <div className="profile-image-small2">
+                          <svg fill="#ffffff" width="40px" height="40px" viewBox="0 0 36 36" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M30.61,24.52a17.16,17.16,0,0,0-25.22,0,1.51,1.51,0,0,0-.39,1v6A1.5,1.5,0,0,0,6.5,33h23A1.5,1.5,0,0,0,31,31.5v-6A1.51,1.51,0,0,0,30.61,24.52Z" class="clr-i-solid clr-i-solid-path-1"></path>
+                            <circle cx="18" cy="10" r="7" class="clr-i-solid clr-i-solid-path-2"></circle>
+                            <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
+                          </svg>
+                        </div>
                         <strong>{admin.firstName} {admin.lastName}</strong>
                       </div>
                     </td>
