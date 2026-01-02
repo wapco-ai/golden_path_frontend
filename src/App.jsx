d82444735634a8df2cd97cb20271c0bada903a4b
wapco <wappo.dev@gmail.com>
2026-01-02 14:47:51 +0330

    Isolate admin language persistence

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ import { Header } from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
 import { ToastContainer, toast } from 'react-toastify';
 import adminRoutes from './routes/adminRoutes';
+import { getPersistedLanguage, useLangStore } from './store/langStore';
 
 const useAppStyles = () => {
   const location = useLocation();
@@ -39,6 +40,8 @@ const useAppStyles = () => {
 const AppContent = () => {
   const location = useLocation();
   const intl = useIntl();
+  const language = useLangStore((state) => state.language);
+  const setLanguage = useLangStore((state) => state.setLanguage);
   const hideHeaderFooter = location.pathname === '/login' || location.pathname === '/profile'|| location.pathname === '/lang'
     || location.pathname === '/location' || location.pathname === '/' || location.pathname === '/mpr'|| location.pathname === '/fs'
     || location.pathname === '/rop' || location.pathname === '/rng'|| location.pathname === '/mpb'
@@ -51,6 +54,15 @@ const AppContent = () => {
 
   const [deferredPrompt, setDeferredPrompt] = useState(null);
   const [showInstall, setShowInstall] = useState(false);
+
+  useEffect(() => {
+    const storedLanguage = getPersistedLanguage(location.pathname);
+    const fallbackLanguage = 'fa';
+    const nextLanguage = storedLanguage || fallbackLanguage;
+    if (nextLanguage !== language) {
+      setLanguage(nextLanguage);
+    }
+  }, [language, location.pathname, setLanguage]);
 
   useEffect(() => {
     document.title = intl.formatMessage({ id: 'appTitle' });
