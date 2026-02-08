7d9e6044eb04535951813a03d81bab80c4909f5d
Wapco-tech <wappo.dev@gmail.com>
2026-02-08 13:25:34 +0330

    ad
    ad

--- src/pages/MapBegin.jsx
+++ src/pages/MapBegin.jsx
@@ -1189,7 +1189,11 @@ const MapBeginPage = () => {
           className="map-navigate-button"
           onClick={() => navigate('/mpr')}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-guide"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 19a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M7 19h3a2 2 0 0 0 2 -2v-8a2 2 0 0 1 2 -2h7" /><path d="M18 4l3 3l-3 3" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" id="directions">
+            <path fill="none" d="M0 0h24v24H0V0z"></path>
+            <path d="M22.43 10.59l-9.01-9.01c-.75-.75-2.07-.76-2.83 0l-9 9c-.78.78-.78 2.04 0 2.82l9 9c.39.39.9.58 1.41.58.51 0 1.02-.19 1.41-.58l8.99-8.99c.79-.76.8-2.02.03-2.82zm-10.42 10.4l-9-9 9-9 9 9-9 9zM8 11v4h2v-3h4v2.5l3.5-3.5L14 7.5V10H9c-.55 0-1 .45-1 1z"></path>
+          </svg>
+
         </button>
         <button
           className={`map-gps-button ${isTracking ? 'active' : 'inactive'}`}
