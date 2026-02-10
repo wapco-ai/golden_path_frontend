bf1ceb7eb12f490e080159bfedec99dc780497af
wapco <wappo.dev@gmail.com>
2026-02-10 17:46:49 +0330

    Revert "Add 'OSMH' offline map option and preview for MPB / MPR"

--- src/components/map/Mprc.jsx
+++ src/components/map/Mprc.jsx
@@ -5,7 +5,6 @@ import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import useOfflineMapStyle from '../../hooks/useOfflineMapStyle';
 import { MBTILES_SATELLITE_STYLE } from '../../services/mbtilesMapStyle';
-import { offlineFallbackStyle } from '../../services/osmMapStyle';
 import { useLangStore } from '../../store/langStore';
 import { getLocationTitleById } from '../../utils/getLocationTitle';
 import { loadGeoJsonData } from '../../utils/loadGeoJsonData.js';
@@ -117,8 +116,7 @@ const Mprc = ({
   const baseMapStyle = isRtl ? "./rtl/style.json" : "./rtl/style-en.json";
   const { mapStyle: offlineMapStyle, handleMapError, styleKey } = useOfflineMapStyle(baseMapStyle);
   const isSatellite = selectedMapType === 'satellite';
-  const isOfflineOsmh = selectedMapType === 'osmh' || selectedMapType === 'simple';
-  const mapStyle = isSatellite ? MBTILES_SATELLITE_STYLE : (isOfflineOsmh ? offlineFallbackStyle : offlineMapStyle);
+  const mapStyle = isSatellite ? MBTILES_SATELLITE_STYLE : offlineMapStyle;
   const mapRenderKey = isSatellite ? 'mbtiles-satellite' : `${styleKey}-${isRtl ? 'rtl' : 'en'}`;
   const vectorTileConfig = useMemo(() => {
     return createHaramVectorTileConfig(language).filter(
