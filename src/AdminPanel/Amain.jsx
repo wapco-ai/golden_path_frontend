49e64276992f3e0d3bdb163c75bc20fe13a9ee5b
gisonline <oskooei@gmail.com>
2026-02-10 15:37:16 +0330

    Update Amain.jsx

--- src/AdminPanel/Amain.jsx
+++ src/AdminPanel/Amain.jsx
@@ -4200,7 +4200,7 @@ const Amain = () => {
 
     const mapInstance = new maplibregl.Map({
       container: 'edit-cultural-map-container',
-      style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
+      style: './rtl/style-en.json',
       center: selectedLocation ?
         [selectedLocation.lng, selectedLocation.lat] :
         [59.6161, 36.2908],
@@ -4611,7 +4611,7 @@ const Amain = () => {
 
     const mapInstance = new maplibregl.Map({
       container: 'cultural-map-container',
-      style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
+      style: './rtl/style-en.json',
       center: [59.6161, 36.2908],
       zoom: 16,
     });
