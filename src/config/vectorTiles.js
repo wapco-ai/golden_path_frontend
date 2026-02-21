e6ed1401ab05fe767c4edba355cd9560bcf6f896
gisonline <oskooei@gmail.com>
2026-02-21 15:17:14 +0330

    Update vectorTiles.js

--- src/config/vectorTiles.js
+++ src/config/vectorTiles.js
@@ -353,7 +353,7 @@ const buildHaramAdminVectorTileConfig = (lang = DEFAULT_TILE_LANG) => {
     visibleByDefault: false,
     layout: {
       // اسم فیلدی که از MVT میاد را اینجا بگذار
-      'text-field': ['coalesce', ['get', 'label'], ['get', 'name'],  ['get', 'id']],
+      'text-field': buildAreaLabelWithIdTextField(),
       'text-size': 12,
       'text-anchor': 'center',
       'text-allow-overlap': false,
